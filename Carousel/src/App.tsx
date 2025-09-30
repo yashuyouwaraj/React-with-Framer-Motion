a6299e98df4a655978f3babc56cbceffd262b4df
Yashu Youwaraj <amanyasu@gmail.com>
2025-09-30 11:40:15 +0530

    Carousel Done

---
+++ Carousel/src/App.tsx
@@ -0,0 +1,12 @@
+import Carousel from "./components/Carousel"
+
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen ">
+      <Carousel />
+    </div>
+  )
+}
+
+export default App
