6c33778ab28fde8f71734ac438b22ed9b1458f0d
Yashu Youwaraj <amanyasu@gmail.com>
2025-10-01 11:21:17 +0530

    Progress Steps

---
+++ Progress Steps/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProgressSteps from './components/ProgressSteps'
+
+const App = () => {
+  return (
+    <div>
+      <ProgressSteps />
+    </div>
+  )
+}
+
+export default App
